24d49953c7ef04a97f48e22605509f402a98e1fb
Lada Kolotova <ladakolotova@MacBook-Pro-3.local>
2024-06-07 22:34:18 +0300

    Short-link

--- backend/recipes/serializers.py
+++ backend/recipes/serializers.py
@@ -171,5 +171,5 @@ class RecipeShortLinkSerializer(serializers.ModelSerializer):
         if obj:
             # hash_object = hashlib.md5(self.context['request'].path.encode())
             # return hash_object.hexdigest()[:8]
-            return str('https://foodgram-ladank.sytes.net/recipes/' + obj.id)
+            return os.path.join(https://foodgram-ladank.sytes.net/recipes/, obj.id)
         return None
