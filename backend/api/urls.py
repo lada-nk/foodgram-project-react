273c883dcfe707445fc35f7588666fdb1118bc22
Lada Kolotova <ladakolotova@MacBook-Pro-3.local>
2024-06-10 16:29:52 +0300

    API

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,11 +4,12 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from .views import (IngredientViewSet, RecipeViewSet,
-                    TagViewSet)
+                    TagViewSet, UserViewSet)
 
-app_name = 'recipes'
+app_name = 'api'
 
 router = DefaultRouter()
+router.register(r'users', UserViewSet, basename='user')
 router.register(r'recipes', RecipeViewSet, basename='recipes')
 router.register(r'ingredients', IngredientViewSet, basename='ingredients')
 router.register(r'tags', TagViewSet, basename='tags')
--- backend/api/views.py
+++ backend/api/views.py
@@ -10,13 +10,16 @@ from rest_framework.exceptions import ValidationError
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
+from recipes.models import Ingredient, Favorite, Recipe, ShoppingCart, Tag
+from users.models import Follow
 from users.serializers import RecipeShortSerializer
 
 from .filters import IngredientFilter, RecipeFilter
-from recipes.models import Ingredient, Favorite, Recipe, ShoppingCart, Tag
+from .pagination import LimitPageNumberPagination
 from .permissions import IsAuthorOrReadOnly
-from .serializers import (IngredientSerializer, RecipieSerializer,
+from .serializers.recipes_serializers import (IngredientSerializer, RecipieSerializer,
                           TagSerializer)
+from .serializers.users_serializers import AvatarSerializer, FollowSerializer, UserSerializer
 
 User = get_user_model()
 
@@ -125,3 +128,67 @@ class TagViewSet(ModelViewSet):
     http_method_names = ['get']
     filter_backends = (DjangoFilterBackend,)
     pagination_class = None
+
+
+class UserViewSet(UserViewSet):
+    """Вьюсет для пользователей."""
+
+    pagination_class = LimitPageNumberPagination
+
+    @action(detail=False, methods=['get'],
+            permission_classes=(permissions.IsAuthenticated,),
+            serializer_class=UserSerializer, url_path='me')
+    def me(self, request, pk=None):
+        return Response(self.get_serializer(request.user).data)
+
+    @action(detail=False, methods=['put', 'patch', 'delete'],
+            permission_classes=(permissions.IsAuthenticated,),
+            url_path='me/avatar')
+    def user_avatar(self, request, pk=None):
+        user = self.request.user
+        if self.request.method == 'DELETE':
+            user.avatar = None
+            user.save()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        serializer = AvatarSerializer(
+            user, data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+    @action(detail=True, methods=['post', 'delete'],
+            permission_classes=(permissions.IsAuthenticated,))
+    def subscribe(self, request, id):
+        user = request.user
+        following = get_object_or_404(User, id=id)
+        if request.method == 'DELETE':
+            follow = Follow.objects.filter(user=user, following=following)
+            if not follow.exists():
+                raise ValidationError(
+                    {'errors': f'Вы не подписаны на автора'
+                     f' {following.username}.'},
+                    code='not_follow_errors')
+            follow.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        if user.id == following.id:
+            raise ValidationError(
+                {'errors': 'Вы не можете быть подписаны на самого себя'},
+                code='id_errors')
+        follow, created = Follow.objects.get_or_create(
+            user=user, following=following)
+        if not created:
+            raise ValidationError(
+                {'errors': f'Вы уже подписаны на автора {following.username}'},
+                code='dublicate_errors')
+        serializer = FollowSerializer(
+            following, context={'request': request})
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    @action(detail=False, methods=['get'],
+            permission_classes=(permissions.IsAuthenticated,))
+    def subscriptions(self, request, pk=None):
+        serializer = FollowSerializer(
+            self.paginate_queryset(
+                [follow.following for follow in request.user.follower.all()]),
+            many=True, context={'request': request})
+        return self.get_paginated_response(serializer.data)
